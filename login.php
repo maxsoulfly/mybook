7dd0a52d07effe82694b35772cc35816c65cceff
maxsoulfly <max.kibinimat@gmail.com>
2025-04-08 23:31:04 +0300

    feat: add styled login section and improve base layout in login.php

--- login.php
+++ login.php
@@ -18,6 +18,20 @@
         <h1>mybook</h1>
         <button class="secondary">Sign Up</button>
     </header>
+
+    <main>
+        <div id="login">
+            <h2>Log in to Mybook</h2>
+            <input type="text" name="email" id="email" placeholder="Enter your email">
+            <input type="password" name="password" id="password" placeholder="Password">
+            <button class="primary">Login</button>
+            <p><a href="#">Forgotten Password?</a><a href="#">Sign Up for Mybook</a></p>
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
 </body>
 
 </html>
