017265e4796b15956d657499ea1639547f9fe5e6
maxsoulfly <max.kibinimat@gmail.com>
2025-04-08 22:58:33 +0300

    feat: add login.php with basic HTML structure, linked header, and start of style.css

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyBook Login</title>
+
+    <!-- Inter Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <header id="header">
+        <h1>mybook</h1>
+        <button class="secondary">Sign Up</button>
+    </header>
+</body>
+
+</html>
