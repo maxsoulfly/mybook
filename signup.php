549213d1741f1f473be1c0b79742420723d1fb0e
maxsoulfly <max.kibinimat@gmail.com>
2025-04-16 16:08:43 +0300

    feat: create signup page

--- signup.php
+++ signup.php
@@ -1,48 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+$page_title = "Sign Up";
+$page = "signup";
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyBook | Sign Up</title>
+include('includes/header.php');
+?>
 
-    <!-- Inter Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+<main>
+    <div id="left-container">
 
-    <link rel="stylesheet" href="css/style.css">
+    </div>
 
-</head>
-
-<body>
-    <header id="header">
-        <h1>MyBook</h1>
-        <button class="secondary">Sign Up</button>
-    </header>
-
-    <main>
-        <div id="left-container">
-
+    <div id="main-container">
+        <div id="login" class="container">
+            <h2>Log in to Mybook</h2>
+            <input type="text" name="email" id="email" placeholder="Enter your email">
+            <input type="password" name="password" id="password" placeholder="Password">
+            <button class="primary">Login</button>
+            <ul class="links">
+                <li><a href="#">Forgotten Password?</a></li>
+                <li><a href="signup.php">Sign Up for Mybook</a></li>
+            </ul>
         </div>
+    </div>
 
-        <div id="main-container">
-            <div id="login" class="container">
-                <h2>Sign Up to Mybook</h2>
-                <input type="text" name="email" id="email" placeholder="Enter your email">
-                <input type="password" name="password" id="password" placeholder="Password">
-                <button class="primary">Login</button>
-                <ul class="links">
-                    <li><a href="#">Forgotten Password?</a></li>
-                    <li><a href="signup.php">Sign Up for Mybook</a></li>
-                </ul>
-            </div>
-        </div>
+    <div id="right-container"></div>
+</main>
 
-        <div id="right-container"></div>
-    </main>
 
-    <footer>
-
-    </footer>
-</body>
-
-</html>
+<?php include('includes/footer.php'); ?>
