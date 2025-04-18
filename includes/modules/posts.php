37d920b467655b03aa498a6490732b50796805a9
maxsoulfly <max.kibinimat@gmail.com>
2025-04-19 00:07:24 +0300

    feat: load posts from associative array and render with foreach + renderPost()

--- includes/modules/posts.php
+++ includes/modules/posts.php
@@ -1,7 +1,29 @@
 <?php include_once __DIR__ . '/../functions.php'; ?>
 
 <?php
-renderPost('Boris Gee', '/assets/img/user1.jpg', 'April 18, 2025', 'Just finished building the profile layout! 💪 PHP next!');
-renderPost('Jane Doe', '/assets/img/user2.jpg', 'April 17, 2025', 'Loving this new view from the mountain. 🌄');
-renderPost('Mike Strong', '/assets/img/user3.jpg', 'April 16, 2025', 'Is it too early to say this might be the best coffee I\'ve ever had?');
+$posts = [ 
+    [
+        'username' => 'Boris Gee',
+        'avatar' => '/assets/img/user1.jpg',
+        'date' => 'April 18, 2025',
+        'content' => 'Just finished building the profile layout!'
+    ],
+    [
+        'username' => 'Jane Doe',
+        'avatar' => '/assets/img/user2.jpg',
+        'date' => 'April 17, 2025',
+        'content' => 'Loving this new view from the mountain. 🌄'
+    ],
+    [
+        'username' => 'Mike Strong',
+        'avatar' => '/assets/img/user3.jpg',
+        'date' => 'April 16, 2025',
+        'content' => 'Is it too early to say this might be the best coffee I\'ve ever had?'
+    ],
+];
+
+foreach ($posts as $post) {
+    renderPost($post['username'], $post['avatar'], $post['date'], $post['content']);
+}
+
 ?>
