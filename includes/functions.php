5bef3e80b6dcc37e737d64346501113a562ca294
maxsoulfly <max.kibinimat@gmail.com>
2025-04-19 00:00:10 +0300

    feat: add renderPost() helper and modularize post output using functions.php

---
+++ includes/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+function renderPost($username, $avatar, $date, $content) {
+    global $BASE_URL;
+
+    echo '
+        <div class="post">
+            <div class="post-header">
+                <img src="'.$BASE_URL. $avatar.'" alt="' . htmlspecialchars($username) . '">
+                <div class="post-user-info">
+                <strong>' . htmlspecialchars($username) . '</strong>
+                <span class="post-date">'. htmlspecialchars($date) .'</span>
+                </div>
+            </div>
+            <p class="post-content">' . htmlspecialchars($content) . '</p>
+            <div class="post-actions">
+                <a href="#">Like</a>
+                <a href="#">Comment</a>
+            </div>
+        </div>
+    ';
+}
