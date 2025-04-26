fb3da004766bf9d14a0c659341e999d2c851a884
maxsoulfly <max.kibinimat@gmail.com>
2025-04-26 09:20:23 +0300

    feat: add temp links to signup and login pages

--- includes/index-nav.php
+++ includes/index-nav.php
@@ -1,5 +1,7 @@
 <nav class="profile-nav">
     <ul>
+        <li><a href="<?= $BASE_URL ?>/pages/signup.php">Sign Up</a></li>
+        <li><a href="<?= $BASE_URL ?>/pages/login.php">login</a></li>
         <li><a href="<?= $BASE_URL ?>/pages/profile.php">Profile</a></li>
         <li><a href="<?= $BASE_URL ?>/pages/timeline.php">Timeline</a></li>
         <li><a href="#">About</a></li>
