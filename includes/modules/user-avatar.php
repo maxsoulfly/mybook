6bbd4c2ccc3f627b74c3c8f5dc801060e1488c03
maxsoulfly <max.kibinimat@gmail.com>
2025-04-28 22:05:56 +0300

    refactor(user-avatar): add username to profile link

--- includes/modules/user-avatar.php
+++ includes/modules/user-avatar.php
@@ -1,5 +1,5 @@
 <div class="avatar">
-    <a href="<?= $BASE_URL ?>/pages/profile.php">
+    <a href="<?= $BASE_URL ?>/pages/profile.php?u=<?= $user["username"] ?>">
         <img src="<?= $user["avatar"] ?>" alt="Profile" class="avatar-small">
         <span class="username"><?= $user["first_name"] . " " . $user["last_name"] ?></span>
     </a>
