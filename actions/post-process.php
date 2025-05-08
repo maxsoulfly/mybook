38d6208fab73b68de4ccc72453fd857625a606e1
maxsoulfly <max.kibinimat@gmail.com>
2025-05-08 14:29:19 +0300

    refactor: clean up post-process.php for better validation and clarity
    - Improved input validation using validateFields().
    - Optimized recipient_id handling with clean conditional.
    - Enhanced code readability and maintainability.

--- actions/post-process.php
+++ actions/post-process.php
@@ -8,45 +8,34 @@ $pdo = getDBConnection();
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     die('Invalid request.');
-} else {
-    if (
-        !empty($_POST['user_id']) &&
-        !empty($_POST['content'])
-    ) {
-        $user_id = filter_input(INPUT_POST,'user_id', FILTER_SANITIZE_NUMBER_INT);
-        $content = htmlspecialchars(trim($_POST['content']), ENT_QUOTES, 'UTF-8');
+}
+$errorMsg = validateFields($_POST);
+if ($errorMsg !== '') {
+    die($errorMsg);
+}
 
-        
-        if (!empty($_POST['recipient_id'])){
-            $recipient_id = filter_input(INPUT_POST, 'recipient_id', FILTER_SANITIZE_NUMBER_INT);
-        }
-        else {
-            $recipient_id = NULL;
-        }
+$user_id = filter_input(INPUT_POST, 'user_id', FILTER_SANITIZE_NUMBER_INT);
+$content = htmlspecialchars(trim($_POST['content']), ENT_QUOTES, 'UTF-8');
 
-        
-        $stmt = $pdo->prepare(
-            "INSERT INTO posts (user_id, content, recipient_id)
-                    VALUES (:user_id, :content, :recipient_id)
+$recipient_id = filter_input(INPUT_POST, 'recipient_id', FILTER_SANITIZE_NUMBER_INT) ?? NULL;
+
+$stmt = $pdo->prepare(
+    "INSERT INTO posts (user_id, content, recipient_id)
+                VALUES (:user_id, :content, :recipient_id)
         "
-        );
-        try {
-            $stmt->execute([
-                'user_id'   => $user_id,
-                'content'      => $content,
-                'recipient_id'   => $recipient_id
-            ]);
+);
+try {
+    $stmt->execute([
+        'user_id'   => $user_id,
+        'content'      => $content,
+        'recipient_id'   => $recipient_id
+    ]);
 
-            $finalUrl = redirectBackWithParam('post', 'success');
-            header('Location: ' . $finalUrl);
-            exit;
-        } catch (PDOException $e) {
-            $finalUrl = redirectBackWithParam('post', 'failed');
-            header('Location: ' . $finalUrl);
-            exit;
-        }
-
-    } else {
-        die('Please fill in all required fields.');
-    }
+    $finalUrl = redirectBackWithParam('post', 'success');
+    header('Location: ' . $finalUrl);
+    exit;
+} catch (PDOException $e) {
+    $finalUrl = redirectBackWithParam('post', 'failed');
+    header('Location: ' . $finalUrl);
+    exit;
 }
